97f2142a96df7e6050c659460873a489915cc8e7
vagrant <vagrant@homestead>
2019-07-01 07:36:49 +0000

    头像分辨率限制

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -11,12 +11,15 @@ class UserRequest extends FormRequest
         return [
             'name' => 'required|between:2,25|unique:users,name,'.Auth::id(),
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,jpg,gif,png|dimensions:min_width=208,min_height=208',
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 208px 以上',
             'name.unique' => '用户名已被占用，请重新填写',
             'name.between' => '用户名必须介于 2 - 25 个字符之间。',
             'name.required' => '用户名不能为空。',
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,7 @@
 <div class="row">
   <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
     <div class="card">
-      <img class="card-img-top" src="https://cdn.learnku.com/uploads/avatars/24929_1560757494.jpeg!/both/400x400" alt="{{ $user->name }}">
+      <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
       <div class="card-body">
         <h5><strong>个人简介</strong></h5>
         <p>{{ $user->introduction }}</p>
