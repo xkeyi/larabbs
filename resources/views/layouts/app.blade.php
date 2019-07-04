853e15871d1899a874f6fe1f04770d8f665c8e60
vagrant <vagrant@homestead>
2019-07-04 08:09:39 +0000

    Simditor 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
   <!-- Styles -->
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+  @yield('styles')
+
 </head>
 <body>
   <div id="app" class="{{ route_class() }}-page">
@@ -36,6 +38,8 @@
 
   <!-- Script -->
   <script src="{{ mix('js/app.js') }}"></script>
+
+  @yield('scripts')
 
 </body>
 </html>
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', '创建话题')
 
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
 @section('content')
   <div class="container">
     <div class="col-md-10 offset-md-1">
@@ -55,4 +59,19 @@
       </div>
     </div>
   </div>
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
 @stop
