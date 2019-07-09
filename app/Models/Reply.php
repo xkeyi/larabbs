bec2ed796319593231718a652a3e111334f177b5
vagrant <vagrant@homestead>
2019-07-09 08:42:14 +0000

    回复列表

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -17,4 +17,10 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
 }
