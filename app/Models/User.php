9057bfb957bf4fbcd6698c75d7a05059109440ab
vagrant <vagrant@homestead>
2019-07-18 08:55:54 +0000

    social authorizations

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar', 'phone'
+        'name', 'email', 'password', 'introduction', 'avatar', 'phone', 'weixin_openid', 'weixin_unionid'
     ];
 
     /**
