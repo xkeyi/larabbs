b9ba141f1e221e186a757fc6c1a82f33d86b494d
vagrant <vagrant@homestead>
2019-07-24 07:38:32 +0000

    topic delete

--- routes/api.php
+++ routes/api.php
@@ -79,6 +79,7 @@ $api->version('v1', [
             // 话题
             $api->post('topics', 'TopicsController@store')->name('api.topics.store');
             $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
+            $api->delete('topics/{topic}', 'TopicsController@destroy')->name('api.topics.destroy');
         });
     });
 });
