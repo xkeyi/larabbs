2bb68893e975fe6d790f79ccf10e92896e4cfad0
vagrant <vagrant@homestead>
2019-07-26 06:26:27 +0000

    permissions index

--- routes/api.php
+++ routes/api.php
@@ -98,6 +98,9 @@ $api->version('v1', [
             $api->get('user/notifications', 'NotificationsController@index')->name('api.user.notifications.index');
             $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
             $api->patch('user/read/notifications', 'NotificationsController@read')->name('api.user.notifications.read');
+
+            // 当前登录用户权限
+            $api->get('user/permissions', 'PermissionsController@index');
         });
     });
 });
