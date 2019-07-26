f1f389ea4c141ca984d77e8b0b3f0164322c3bdf
vagrant <vagrant@homestead>
2019-07-26 07:38:11 +0000

    links index

--- routes/api.php
+++ routes/api.php
@@ -74,6 +74,9 @@ $api->version('v1', [
         $api->get('topics/{topic}/replies', 'RepliesController@index')->name('api.topics.replies.index');
         $api->get('users/{user}/replies', 'RepliesController@userIndex')->name('api.users.replies.index');
 
+        // 推荐资源
+        $api->get('links', 'LinksController@index')->name('api.links.index');
+
         /** 需要 token 验证的接口 */
         // 中间件也可以使用 auth:api，但是这个验证不通过时返回 500 而不是 401
         // 在控制器中获取用户 auth:api(框架自带): auth('api')->user();api.auth(dingo/api)：$this->user()
