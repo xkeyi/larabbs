c148603b83cc5f91ffa7377a3b8077c08c16c762
vagrant <vagrant@homestead>
2019-08-01 08:24:06 +0000

    weapp user store

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -74,4 +74,48 @@ class UsersController extends Controller
 
         return $this->response->collection($users, new UserTransformer());
     }
+
+    public function weappStore(UserRequest $request)
+    {
+         $verifyData = Cache::get($request->verification_key);
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
+            // 返回 401
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        // 获取微信的 openid 和 session_key
+        $miniProgram = \EasyWeChat::miniProgram();
+        $data = $miniProgram->auth->session($request->code);
+
+        // 如果 openid 对应的用户已存在，报错 403
+        $user = User::where('weapp_openid', $data['openid'])->first();
+
+        if ($user) {
+            return $this->response->errorForbidden('微信已绑定其他用户，请直接登录');
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+            'weapp_openid' => $data['openid'],
+            'weixin_session_key' => $data['session_key'],
+        ]);
+
+        // 清楚验证码缓存
+        Cache::forget($request->verification_key);
+
+        // return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+                            ->setMeta([
+                                'access_token' => Auth::guard('api')->fromUser($user),
+                                'token_type' => 'Bearer',
+                                'expires_in' => Auth::guard('api')->factory()->getTTl() * 60,
+                            ])
+                            ->setStatusCode(201);
+    }
 }
